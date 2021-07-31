8200f7552e8a56bf84e92fd409b0c226fb97031a
vansh705 <73578776+vansh705@users.noreply.github.com>
2021-07-31 15:30:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function fakeip{
+var fakeip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
+return fakeip
+            }
+module.exports = fakeip
